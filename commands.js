1082a4345f3a146fa3a36cef65429299f51f2da7
Joe Lee Official <70639311+joeleeofficial@users.noreply.github.com>
2021-01-16 20:34:42 +0800

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,116 @@
+/* Coded By JoeLeeofficial <tojoeleeofficial@gmail.com> */
+
+document.getElementById("commands").innerHTML = `
+
+
+
+<!-- help commands -->
+<div class="container">
+
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" style="text-decoration:none" href="#collapse1" >Help Commands</a>
+        </h4>
+      </div>
+      <div id="collapse1" class="panel-collapse collapse">
+        <div class="panel-body" style="color:black">-help ⚙ [ Main Help Command ]</div>
+       <div class="panel-body" style="color:black">-help utility  🛠  [ Utlity Help Command ]
+</div>
+ <div class="panel-body" style="color:black">-help music  🎶  [ Music Help Command ]
+</div>
+<div class="panel-body" style="color:black">-help leveling  🔰  [ Leveling Help Command ]
+</div>
+<div class="panel-body" style="color:black">-help bot  🤖  [ Bot Help Command ]
+</div>
+<div class="panel-body" style="color:black">-help fun  🕹  [ Fun Help Command ]
+</div>
+<div class="panel-body" style="color:black">-help images  🖼  [ Image Help Command ]
+</div>
+<div class="panel-body" style="color:black">-help bs  🎮  [ Brawl Stars Help Command ]
+</div>
+<div class="panel-body" style="color:black">-help cr  🎮  [ Clash Royale Help Command ]
+</div>
+<div class="panel-body" style="color:black">-help coc  🎮  [ Clash Of Clans Help Command ] 
+</div>
+      </div>
+    </div>
+  </div>
+</div>
+  
+
+
+
+<!-- utlity commands -->
+  <div class="container">
+
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" href="#collapse2" style="text-decoration:none">Utility Commands</a>
+        </h4>
+      </div>
+      <div id="collapse2" class="panel-collapse collapse">
+        <div class="panel-body" style="color:black">-c.invite [ Create A Permanent ]</div>
+        <div class="panel-footer" style="color:black"><div class="x">-source
+  <span class="y">-source [Query]</span>
+</div> [ Share Something On Sourcebin ]</div>
+<div class="panel-footer" style="color:black"><div class="x">-source.v
+  <span class="y">-source.v [Query]</span>
+</div> [ Share Something On Sourcebin And View It ]</div>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+`;
+
+
+
+
+
+
+
+// add collum : <div class="panel-body" // style="color:black">-help //utility    [ Utlity Help Command ]
+// </div>
+
+
+/* add container : <div class="container">
+
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" style="text-decoration:none" href="#collapse1" >Help Commands</a>
+        </h4>
+      </div>
+      <div id="collapse1" class="panel-collapse collapse">
+        <div class="panel-body" style="color:black">-help  [ Main Help Command ]</div>
+       <div class="panel-body" style="color:black">-help utility    [ Utlity Help Command ]
+</div>
+ <div class="panel-body" style="color:black">-help utility    [ Utlity Help Command ]
+</div>
+      </div>
+    </div>
+  </div>
+</div> */
+
+
+/*
+
+add hover to display : <div class="x">Hover over me
+  <span class="y">Tooltip text</span>
+</div> 
+
+*/
