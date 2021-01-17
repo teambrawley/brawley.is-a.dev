08ae858c079192d9a8155a7c33c1d62244ac45c4
Joe Lee Official <70639311+joeleeofficial@users.noreply.github.com>
2021-01-17 20:23:50 +0800

    Update commands.js

--- commands.js
+++ commands.js
@@ -11,7 +11,7 @@ document.getElementById("commands").innerHTML = `
     <div class="panel panel-default">
       <div class="panel-heading">
         <h4 class="panel-title">
-          <a data-toggle="collapse" style="text-decoration:none" href="#collapse1" >Help Commands</a>
+          <a data-toggle="collapse" style="text-decoration:none" href="#collapse1" >⚙ Help Commands ⚙</a>
         </h4>
       </div>
       <div id="collapse1" class="panel-collapse collapse">
@@ -49,17 +49,211 @@ document.getElementById("commands").innerHTML = `
     <div class="panel panel-default">
       <div class="panel-heading">
         <h4 class="panel-title">
-          <a data-toggle="collapse" href="#collapse2" style="text-decoration:none">Utility Commands</a>
+          <a data-toggle="collapse" href="#collapse2" style="text-decoration:none">🛠 Utility Commands 🛠</a>
         </h4>
       </div>
       <div id="collapse2" class="panel-collapse collapse">
         <div class="panel-body" style="color:black">-c.invite [ Create A Permanent ]</div>
         <div class="panel-footer" style="color:black"><div class="x">-source
-  <span class="y">-source [Query]</span>
+  <span class="y"> -source [Query] </span>
 </div> [ Share Something On Sourcebin ]</div>
 <div class="panel-footer" style="color:black"><div class="x">-source.v
-  <span class="y">-source.v [Query]</span>
+  <span class="y"> -source.v [Query] </span>
 </div> [ Share Something On Sourcebin And View It ]</div>
+
+
+<div class="panel-body" style="color:black">
+<div class="x">-stats
+  <span class="y">-stats [@user]</span>
+</div>  [ Get A User Profile Stats ]
+</div>
+
+<div class="panel-body" style="color:black">
+<div class="x">-kick
+  <span class="y">-kick [@user]</span>
+</div>  [ Kick A Member (User)] + Admin Only
+</div>
+
+
+<div class="panel-body" style="color:black">
+<div class="x">-g.u
+  <span class="y">-g.u [username]</span>
+</div> [ Get A Github User's Profile ] 
+</div>
+
+<div class="panel-body" style="color:black">
+<div class="x">-time
+  <span class="y">-time [timezone]</span>
+</div> [ Get Global Time ] 
+</div>
+
+<div class="panel-body" style="color:black">
+<div class="x">-stock
+  <span class="y">-stock [stock_quote]</span>
+</div> [ Get Stock Quote Data ] 
+</div>
+
+<div class="panel-body" style="color:black">
+<div class="x">-urban
+  <span class="y">-urban [Query]</span>
+</div> [ Search For Slang Words On The Urban Dictionary ] 
+</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- music commands -->
+
+
+<div class="container">
+
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" style="text-decoration:none" href="#collapse3" >🎶 Music Commands 🎶</a>
+        </h4>
+      </div>
+      <div id="collapse3" class="panel-collapse collapse">
+      <div class="panel-body" style="color:black">
+<div class="x">-play
+  <span class="y">-play [Query]</span>
+</div> [ Play A Song From YouTube ] 
+</div>
+       <div class="panel-body" style="color:black">-stop [ Stop The Player ]
+</div>
+ <div class="panel-body" style="color:black">
+<div class="x">Information Hover To Read
+  <span class="y">Join Voice Channel Before Running Any Music Command</span>
+</div> 
+</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- Brawl Stars commands -->
+<div class="container">
+
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" style="text-decoration:none" href="#collapse8" >🎮 Brawl Stars Commands 🎮</a>
+        </h4>
+      </div>
+      <div id="collapse8" class="panel-collapse collapse">
+        <div class="panel-body" style="color:black">-profile {Tag} [ Get A Player's Profile Stats ]</div>
+       <div class="panel-body" style="color:black">-club {Tag} [ Get A Club Stats]
+</div>
+ <div class="panel-body" style="color:black">-battle {Tag} [ Get A Player's Battle Log ]
+</div>
+ <div class="panel-body" style="color:black">-battle2 {Tag} [ Get A Player's Second Battle Log ]
+</div>
+ <div class="panel-body" style="color:black">-battle3 {Tag} [ Get A Player's Third Battle Log ]
+</div>
+ <div class="panel-body" style="color:black">-battle4 {Tag} [ Get A Player's Fourth Battle Log ]
+</div>
+ <div class="panel-body" style="color:black">-battle5 {Tag} [ Get A Player's Fifth Battle Log ]
+</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Clash Of Clans commands -->
+<div class="container">
+
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" style="text-decoration:none" href="#collapse9" >🎮 Clash Of Clans Commands 🎮</a>
+        </h4>
+      </div>
+      <div id="collapse9" class="panel-collapse collapse">
+        <div class="panel-body" style="color:black">-profile.coc {Tag} [ Get A Player's Profile Stats ]</div>
+       <div class="panel-body" style="color:black">-coc {Tag} [ Get A Clan Stats ]
+</div>
+ 
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Clash Royale commands -->
+<div class="container">
+
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" style="text-decoration:none" href="#collapse10" >🎮 Clash Royale Commands 🎮</a>
+        </h4>
+      </div>
+      <div id="collapse10" class="panel-collapse collapse">
+        <div class="panel-body" style="color:black">-profile.cr {Tag} [ Get A Player's Profile Stats ]</div>
+       <div class="panel-body" style="color:black">-clan {Tag} [ Get A Clan Stats ]
+</div>
+ 
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<!-- bot commands -->
+<div class="container">
+
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" style="text-decoration:none" href="#collapse5" >🤖 Bot Commands 🤖</a>
+        </h4>
+      </div>
+      <div id="collapse5" class="panel-collapse collapse">
+      <div class="panel-body" style="color:black">-ping [ Returns Pong! ]</div>
+       <div class="panel-body" style="color:black">-status [ Check Bot's Status ]
+</div>
+ <div class="panel-body" style="color:black">-uptime [ Check Bot's Uptime ]
+</div>
+ <div class="panel-body" style="color:black">-info [ Information About Brawley ]
+</div>
+ <div class="panel-body" style="color:black">-support [ Support Server ]
+</div>
+ <div class="panel-body" style="color:black">-vote [ Vote Our Bot ]
+</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- fun commands -->
+<div class="container">
+
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" style="text-decoration:none" href="#collapse6" >🕹 Fun Commands 🕹</a>
+        </h4>
+      </div>
+      <div id="collapse6" class="panel-collapse collapse">
+
+       <div class="panel-body" style="color:black">-8ball [ Ask 8ball Something ]
+</div>
+ <div class="panel-body" style="color:black">-flip [ Flip A Coin ]
+</div>
+ <div class="panel-body" style="color:black">-rock [ Play Rock Paper Scissors ]
+</div>
+ <div class="panel-body" style="color:black">-roll [ Roll A Dice ]
+</div>
 
       </div>
     </div>
@@ -70,6 +264,51 @@ document.getElementById("commands").innerHTML = `
 
 
 
+<!-- images commands -->
+<div class="container">
+
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" style="text-decoration:none" href="#collapse7" >🖼 Images Commands 🖼</a>
+        </h4>
+      </div>
+      <div id="collapse7" class="panel-collapse collapse">
+        <div class="panel-body" style="color:black">-!top.gg {BOT_ID} [ Generates A Top.gg Bot Widget ] </div>
+       <div class="panel-body" style="color:black">-!repo <USERNAME> {REPO_NAME} [ Generates A Github Repo Widget Link ]</div>
+       <div class="panel-body" style="color:black">-!g.stats {USERNAME} [ Generates A Github User's Stats Widget ]</div>
+       <div class="panel-body" style="color:black">-! {QUERY} [ ✨✨ ]</div>
+      
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<!-- leveling commands -->
+<div class="container">
+
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" style="text-decoration:none" href="#collapse4" >🔰 Leveling Commands 🔰</a>
+        </h4>
+      </div>
+      <div id="collapse4" class="panel-collapse collapse">
+        <div class="panel-body" style="color:black">-rank [ Check Your Rank/Level ] </div>
+       <div class="panel-body" style="color:black">-top  [ Leaderboard ]</div>
+ <div class="panel-body" style="color:black">
+<div class="x">Information Hover To Read
+  <span class="y">You Can Earn Xp By Using Brawley's Command. Level Up After Reaching Certain Amount Of Xp.</span>
+</div>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 
